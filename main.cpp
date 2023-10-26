c3270baa34773dff0913785343def24962fdd669
yymin1022 <yymin1022@gmail.com>
2023-10-26 20:58:22 +0900

    Re-initialized Project with VS 2022

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-int main(){
-    std::cout << "Hello, World!" << "\n";
+int main(int argc, char** argv) {
+	std::cout << "Hello, World!" << std::endl;
 
-    return 0;
+	return 0;
 }
