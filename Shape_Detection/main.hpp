8c03ff130c57aaeb95946f84f205c07926b6bd46
yymin1022 <yymin1022@gmail.com>
2023-10-27 03:39:00 +0900

    Update ShapeDetection : Added Function Prototypes

--- Shape_Detection/main.hpp
+++ Shape_Detection/main.hpp
@@ -1,9 +1,14 @@
 #ifndef _MAIN_HPP
 #define _MAIN_HPP
 
-#include <iostream>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/videoio.hpp>
-#include <opencv2/highgui.hpp>
+#include <string>
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+using namespace std;
+
+void showLabel(Mat& img, string str, vector<Point> contour);
+void imageColorToGray(Mat& org, Mat& res);
+void imageGrayToBin(Mat& org, Mat& res);
 
 #endif
