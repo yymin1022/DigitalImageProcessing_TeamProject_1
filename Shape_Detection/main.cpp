8c3e25f5a2f8896645d4d67ff06b0345005c38b4
yymin1022 <yymin1022@gmail.com>
2023-10-27 02:40:20 +0900

    Update ShapeDetection : Added Example OpenCV Code

--- Shape_Detection/main.cpp
+++ Shape_Detection/main.cpp
@@ -1,7 +1,12 @@
 #include "main.hpp"
 
-int main(int argc, char** argv) {
-	std::cout << "Hello, World!" << std::endl;
-	
-	return 0;
+using namespace cv;
+
+int main(int ac, char** av) {
+
+    Mat img = imread("Result.png");
+    imshow("img", img);
+    waitKey(0);
+
+    return 0;
 }
--- Shape_Detection/main.hpp
+++ Shape_Detection/main.hpp
@@ -2,5 +2,8 @@
 #define _MAIN_HPP
 
 #include <iostream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
 
 #endif
